842add8edc4930825eac4b1d663ea85527bccc80
rclaver <rclaver@xtec.cat>
2026-01-14 20:15:46 +0100

    pas 2

--- veus.py
+++ veus.py
@@ -18,6 +18,7 @@ import tkinter as tk
 from tkinter import ttk
 import torch
 from TTS.api import TTS
+from pydub import AudioSegment
 from pydub.playback import play
 #import speech_recognition as sr
 #import wave
@@ -31,7 +32,9 @@ class MostraDeVeus:
 
       # Variables
       self.arxiu_wav = "tmp/tmp.wav"
+      self.arxiu_sortida = "tmp/llista_de_veus.txt"
       self.selected_voice = tk.StringVar(value="")
+      self.veu_actual = tk.StringVar(value="")
       self.dir_images = "static/img"
       self.images = {}
       self.tts = None
@@ -47,6 +50,7 @@ class MostraDeVeus:
       self.images['anterior'] = tk.PhotoImage(file=f"{self.dir_images}/anterior.png")
       self.images['inici'] = tk.PhotoImage(file=f"{self.dir_images}/inici.png")
       self.images['seguent'] = tk.PhotoImage(file=f"{self.dir_images}/seguent.png")
+      self.images['desar'] = tk.PhotoImage(file=f"{self.dir_images}/desar.png")
       self.images['sortir'] = tk.PhotoImage(file=f"{self.dir_images}/sortir.png")
 
    def carrega_veus(self):
@@ -80,63 +84,71 @@ class MostraDeVeus:
          values=self.voices,
          state="readonly",
          font=("Arial",9),
-         width=16
+         width=70
       )
-      self.voice_combo.grid(row=0, column=0, sticky=tk.W, padx=(0, 10))
-
-      # Etiqueta que mostra el codi de la veu seleccionada
-      self.veu_actual = ttk.Label(
-         voice_frame,
-         text=f"veu actual: {self.selected_voice.get()}",
-         font=("Arial", 10),
-         foreground="#0000a0"
-      )
-      self.veu_actual.grid(row=0, column=1, sticky=tk.W)
+      self.voice_combo.grid(row=0, column=0, sticky=tk.W, padx=5)
 
       # Vincular l'event de canvi de selecció
       self.voice_combo.bind('<<ComboboxSelected>>', self.on_voice_change)
 
+      # Etiqueta que mostra el codi de la veu seleccionada
+      ttk.Label(main_frame, textvariable=self.veu_actual, font=("Arial",9)).grid(row=2, column=0, columnspan=3, sticky=(tk.N,tk.W))
+
       # Botons de control
       button_frame = ttk.Frame(main_frame)
-      button_frame.grid(row=2, column=0, columnspan=3, sticky=tk.N, pady=(15,0))
+      button_frame.grid(row=3, column=0, columnspan=3, sticky=tk.N, pady=(30,0))
 
       ttk.Button(button_frame, image=self.images['anterior'], command=self.anterior).pack(side=tk.LEFT, padx=5)
       ttk.Button(button_frame, image=self.images['inici'], command=self.text_to_audio).pack(side=tk.LEFT, padx=5)
       ttk.Button(button_frame, image=self.images['seguent'], command=self.seguent).pack(side=tk.LEFT, padx=5)
-      ttk.Button(button_frame, image=self.images['sortir'], command=self.root.destroy).pack(side=tk.LEFT, padx=(10,0))
+      ttk.Button(button_frame, image=self.images['desar'], command=self.desar).pack(side=tk.LEFT, padx=(15,0))
+      ttk.Button(button_frame, image=self.images['sortir'], command=self.root.destroy).pack(side=tk.LEFT, padx=(15,0))
 
+
+   def text_to_audio(self):
+      self.mostra_veu_actual()
+      #print("tts: ", self.tts)
+      #print("tts.speakers: ", self.tts.speakers)
+
+      # Text to speech list of amplitude values as output
+      #wav = self.tts.tts(self.text, speaker=self.voices[self.n_voice])
+      #play(wav)
+
+      # Text to speech to a file
+      self.tts.tts_to_file(self.text, speaker=self.voices[self.n_voice], file_path=self.arxiu_wav, verbose=False)
+      audio_pendent = AudioSegment.from_wav(self.arxiu_wav)
+      play(audio_pendent)
+
+   def anterior(self):
+      self.n_voice -= 1
+      self.text_to_audio()
+
+   def seguent(self):
+      self.n_voice += 1
+      self.text_to_audio()
+
+   def desar(self):
+      """Desa el nom de la veu actual en un arxiu de text"""
+      try:
+         with open(self.arxiu_sortida, 'a', encoding='utf-8') as file:
+            file.write(self.voices[self.n_voice])
+      except Exception as e:
+         self.veu_actual.set(f"Error en desar: {str(e)}")
 
    def on_voice_change(self, event):
       '''Actualitza l'etiqueta de la veu quan canvia la selecció'''
       selected_voice_name = self.voice_combo.get()
       self.selected_voice.set(selected_voice_name)
-      self.veu_actual.config(text=f"veu actual: {selected_voice_name}")
+      self.veu_actual.set(selected_voice_name)
+
+      self.n_voice = self.voices.index(selected_voice_name)
+      self.text_to_audio()
 
    def mostra_veu_actual(self):
       '''Actualitza l'etiqueta de la veu'''
+      self.veu_actual.set(self.voices[self.n_voice])
       self.selected_voice.set(self.voices[self.n_voice])
-      self.veu_actual.config(text=f"veu actual: {self.voices[self.n_voice]}")
-
-   def text_to_audio(self):
-      #print("tts: ", self.tts)
-      #print("tts.speakers: ", self.tts.speakers)
-
-      # Text to speech list of amplitude values as output
-      wav = self.tts.tts(self.text, speaker=self.voices[self.n_voice])
-      play(wav)
-
-      # Text to speech to a file
-      self.tts.tts_to_file(self.text, speaker=self.voices[self.n_voice], file_path=self.arxiu_wav, verbose=False)
-      #audio_pendent = AudioSegment.from_wav(self.arxiu_wav)
-      #play(audio_pendent)
-
-   def anterior(self):
-      self.n_voice -= 1
-      self.mostra_veu_actual()
-
-   def seguent(self):
-      self.n_voice += 1
-      self.mostra_veu_actual()
+      self.voice_combo.set(self.voices[self.n_voice])
 
 
 if __name__ == "__main__":
